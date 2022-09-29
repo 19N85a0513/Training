982ffd416bc44a099dd6d3af4a6713f75b24845c
Kavitha Pisati <kavithap@infinite.com>
2022-09-29 19:31:53 +0530

    files

---
+++ Csharp/Assignment1/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str;
+            String rev = "";
+            Console.WriteLine(" Enter the word:");
+            str = Console.ReadLine();
+            for(int i=str.Length-1;i>=0;i--)
+            {
+                rev += str[i].ToString();
+            }
+            if(rev== str)
+            {
+                Console.WriteLine("word is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("string is not a palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp/Assignment1/Positivenumber/Positivenumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Positivenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("\n\n");
+            Console.Write("check whether a number is positive or negative:\n");
+            Console.Write("----------------");
+            Console.Write("Enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("{0} is a positive number \n", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a negative number \n", num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
