573a25730b87142915bf8058e29968873dddbb87
19N85a0513 <114407221+19N85a0513@users.noreply.github.com>
2022-10-20 06:40:42 +0530

    Add files via upload

---
+++ Exam/Exam/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Box
+    {
+        public void add2objects(int length, int breadth)
+        {
+            int c = length + breadth;
+            Console.WriteLine(c);
+            Console.Read();
+        }
+    }
+    class Test : Box
+    {
+        public static void Main()
+        {
+            Test t = new Test();
+            t.add2objects(4, 6);
+            Console.Read();
+
+        }
+    }
+}
+  
+     
